528dba6a34a0264857fd03fdd8f61f9fdb74f728
sciemniak000 <sciemniak000@gmail.com>
2020-06-21 10:22:50 +0200

    Added menu button

--- app/src/main/java/agh/edu/pl/inf3/mobiapp/MainActivity.kt
+++ app/src/main/java/agh/edu/pl/inf3/mobiapp/MainActivity.kt
@@ -6,9 +6,8 @@ import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import agh.edu.pl.inf3.mobiapp.ui.main.SectionsPagerAdapter
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,6 +19,13 @@ class MainActivity : AppCompatActivity() {
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
+
+        val menu: ImageButton = findViewById(R.id.menu_button)
+        menu.setOnClickListener {view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
         val fab: FloatingActionButton = findViewById(R.id.fab)
 
         fab.setOnClickListener { view ->
