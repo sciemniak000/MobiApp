4c42e7d86eb363905e1af19f357678c204c6f54a
sciemniak000 <sciemniak000@gmail.com>
2020-09-14 22:43:53 +0200

    Database management class

---
+++ app/src/main/java/agh/edu/pl/inf3/mobiapp/DatabaseUtils.java
@@ -0,0 +1,10 @@
+package agh.edu.pl.inf3.mobiapp;
+
+import android.graphics.Bitmap;
+import android.media.Image;
+
+public class DatabaseUtils {
+    public static Bitmap loadTomorrowImage(TomorrowImage enm){
+        return null;
+    }
+}
---
+++ app/src/main/java/agh/edu/pl/inf3/mobiapp/TomorrowImage.java
@@ -0,0 +1,16 @@
+package agh.edu.pl.inf3.mobiapp;
+
+public enum TomorrowImage {
+    H01,
+    H02,
+    H03,
+    H04,
+    H05,
+    H06,
+    H07,
+    H08,
+    H09,
+    H10,
+    H11,
+    H12
+}
